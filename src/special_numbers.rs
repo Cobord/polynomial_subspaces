ee737b47e83e6a9e6e0e2b650957035ea8a0e65f
Cobord <ammar.s.husain@gmail.com>
2024-08-20 14:00:33 -0400

    the needed special numbers in jacobi for both f32,f64

--- src/special_numbers.rs
+++ src/special_numbers.rs
@@ -10,5 +10,13 @@ impl SpecialNumbers for f64 {
 
     const PI: Self = std::f64::consts::PI;
 
-    const SQRT_PI: Self = todo!();
+    const SQRT_PI: Self = 2.0 / std::f64::consts::FRAC_2_SQRT_PI;
+}
+
+impl SpecialNumbers for f32 {
+    const SQRT_TWO: Self = std::f32::consts::SQRT_2;
+
+    const PI: Self = std::f32::consts::PI;
+
+    const SQRT_PI: Self = 2.0 / std::f32::consts::FRAC_2_SQRT_PI;
 }
